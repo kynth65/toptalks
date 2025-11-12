d7e26db0a89df56de282a72a17862d29895d03c3
kynth65 <kynth65@gmail.com>
2025-11-12 18:19:20 +0800

    Implement routing with language provider integration

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import PricingPage from './pages/PricingPage'
+import LoginPage from './pages/LoginPage'
+import { LanguageProvider } from './contexts/LanguageContext'
 
 function App() {
-  return <LandingPage />
+  return (
+    <LanguageProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </LanguageProvider>
+  )
 }
 
 export default App
