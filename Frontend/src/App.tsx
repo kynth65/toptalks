fa3d529774a0abd08805d5f5f563b779f9932df0
kynth65 <kynth65@gmail.com>
2025-11-13 10:28:32 +0800

    Add routes for new pages in router configuration

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,8 +3,11 @@ import LandingPage from './pages/LandingPage'
 import PricingPage from './pages/PricingPage'
 import LoginPage from './pages/LoginPage'
 import StudentServicesPage from './pages/StudentServicesPage'
+import StartLearningPage from './pages/StartLearningPage'
 import StartTeachingPage from './pages/StartTeachingPage'
+import TutorApplicationPage from './pages/TutorApplicationPage'
 import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
 import { LanguageProvider } from './contexts/LanguageContext'
 import ScrollToTop from './components/common/ScrollToTop'
 
@@ -17,9 +20,12 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/contact" element={<ContactPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/student-services" element={<StudentServicesPage />} />
+          <Route path="/start-learning" element={<StartLearningPage />} />
           <Route path="/start-teaching" element={<StartTeachingPage />} />
+          <Route path="/apply-tutor" element={<TutorApplicationPage />} />
         </Routes>
       </BrowserRouter>
     </LanguageProvider>
